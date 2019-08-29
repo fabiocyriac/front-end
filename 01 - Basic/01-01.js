4fe9c34434bf44ac0ed1c8360029708a8c452869
fcyriac <fabiocyriac123@gmail.com>
2019-08-29 15:54:52 -0700

    updated with function call

--- 01 - Basic/01-01.js
+++ 01 - Basic/01-01.js
@@ -6,16 +6,20 @@ Current time is : 4 PM : 50 : 22
 
 "use strict";
 
-(function getCurrentDay() {
+function getCurrentDay() {
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var current = new Date();
     console.log("Today is " + days[current.getDay()] + ".");
-})();
+}
 
-(function getCurrentTime() {
+getCurrentDay();
+
+function getCurrentTime() {
     var current = new Date();
     var hours = current.getHours() > 12 ? current.getHours() - 12 + " PM" : current.getHours() + " AM";
     var minutes = current.getMinutes();
     var seconds = current.getSeconds();
     console.log("Current time is: " + hours + " : " + minutes + " : " + seconds + ".");
-})();         
+}
+
+getCurrentTime();             
